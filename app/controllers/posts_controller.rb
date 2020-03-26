9726ec7c5ada0c7f13196c5be81c7184a14118db
islamfelfela <islamsalahh@gmail.com>
2020-03-27 01:33:39 +0200

    adding crud  functins

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,42 @@
 class PostsController < ApplicationController
-    def new
+    def show
+
     end
 
+    def new
+        @post = Post.new
+    end
+
+
     def index
+        @posts = Post.all
     end
 
     def create
-        # @post = Post.new(params[:post])
- 
-        # @post.save
-        # redirect_to @post
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to :posts
     end
 
-    # private
-    #     def post_params
-    #         params.require(:post).permit(:title, :content)
-    #     end
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to action: :index
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy!
+        redirect_to action: :index
+    end
+
+
+    private
+        def post_params
+            params.permit(:title, :content)
+        end
 end
